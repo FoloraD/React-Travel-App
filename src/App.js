734409ad0b3dd53013406363c99048981a866c43
folorad <fduang@gmail.com>
2022-09-15 16:55:44 +0100

    added styling to header component

--- src/App.js
+++ src/App.js
@@ -12,12 +12,10 @@ const App = () => {
 
             <Grid container spacing={3} style={{ width: '100%'}}> 
                 <Grid item xs={12} md={4}></Grid>
-                    <List />
-            </Grid>
-
-            <Grid container spacing={3} style={{ width: '100%'}}> 
-                <Grid item xs={12} md={8}></Grid>
                     <Map />
+            </Grid>
+            <Grid item xs={12} md={8}>
+                    <List />
             </Grid>
         
         </>
