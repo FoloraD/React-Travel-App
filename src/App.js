4fe61ba8d9fdc262f2a1e9edeccb11fdce5724b1
folorad <fduang@gmail.com>
2022-09-15 12:01:36 +0100

    Added componets: Header, List, Map

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import { CssBaseline, Grid } from '@material-ui/core';
+import Header from './components/Header/Header';
+import List from './components/List/List';
+import Map from './components/Map/map';
 
 const App = () => {
     return (
-        <div>
-            <h1> Travel Advisor App</h1>
-        </div>
+        <>
+            <CssBaseline />
+            <Header />
+
+            <Grid container spacing={3} style={{ width: '100%'}}> 
+                <Grid item xs={12} md={4}></Grid>
+                    <List />
+            </Grid>
+
+            <Grid container spacing={3} style={{ width: '100%'}}> 
+                <Grid item xs={12} md={8}></Grid>
+                    <Map />
+            </Grid>
+        
+        </>
     );
 }
 
