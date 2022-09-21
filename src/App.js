023a26c0782d6a1746ae7d1cc46dd50da345cabf
folorad <fduang@gmail.com>
2022-09-21 14:42:19 +0100

    Added css styling to List & PlaceDetails components

--- src/App.js
+++ src/App.js
@@ -9,13 +9,13 @@ const App = () => {
         <>
             <CssBaseline />
             <Header />
-
             <Grid container spacing={3} style={{ width: '100%'}}> 
-                <Grid item xs={12} md={4}></Grid>
-                    <Map />
-            </Grid>
-            <Grid item xs={12} md={8}>
+                <Grid item xs={12} md={4}>
                     <List />
+                </Grid >
+                <Grid item xs={12} md={8}>
+                    <Map />
+                </Grid>       
             </Grid>
         
         </>
