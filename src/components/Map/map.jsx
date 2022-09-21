37b6253fd5d6c18a288bb94a66988d865cbeaba4
folorad <fduang@gmail.com>
2022-09-21 13:29:37 +0100

    added map component to file

--- src/components/Map/map.jsx
+++ src/components/Map/map.jsx
@@ -1,8 +1,40 @@
 import React from 'react';
+import GoogleMapReact from "google-map-react";
+import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
+import { Paper, Typography, useMediaQuery } from '@mui/material';
+import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined"
+import  Rating  from '@material-ui/lab/Rating';
 
-const Map = () => {
+import useStyles from './styles.js'
+import mapStyles from './mapStyles.js';
+
+const Map = ({setCoordinates, setBounds, coordinates, places, setChildClicked }) => {
+    const classes = useStyles();
+    const matches = useMediaQuery('(min-width:600px)');
+
+
     return (
-        <h1>Map</h1>
+        <div className={classes.mapContainer}>
+            <GoogleMapReact
+                bootstrapURLKeys={{ key: process.env.REACT_APP_MAP_API_KEY }}
+                defaultCenter={coordinates}
+                center={coordinates}
+                defaultZoom={14}
+                margin={[50, 50, 50, 50]}
+                options={{ disableDefaulUI: true, zoomControl:true, styles: mapStyles }}
+                onChange={(e) => {
+
+                    setCoordinates({ lat: e.center.lat, lng: e.center.lng });
+                    setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
+                }}
+                onChildClick={(child)=> setChildClicked(child) }
+            >
+                <h1>Map</h1>
+            
+
+            </GoogleMapReact>
+
+        </div>
     );
 }
 
