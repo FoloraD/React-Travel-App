c2a7b656a7bf38bd6bc0365a1d1d5a20de40467e
folorad <fduang@gmail.com>
2022-09-22 15:05:57 +0100

    refactored functions

--- src/components/Map/map.jsx
+++ src/components/Map/map.jsx
@@ -1,66 +1,93 @@
-import React from 'react';
+import React from "react";
 import GoogleMapReact from "google-map-react";
-import { Paper, Typography, useMediaQuery } from '@mui/material';
-import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined"
-import  Rating  from '@material-ui/lab/Rating';
+import { Paper, Typography, useMediaQuery } from "@mui/material";
+import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
+import Rating from "@material-ui/lab/Rating";
 
-import useStyles from './MStyles.js'
-import mapStyles from './mapStyles.js';
+import useStyles from "./MStyles.js";
+import mapStyles from "./mapStyles.js";
 
-const Map = ({setCoords, setBounds, coords, places, setChildClicked, weatherData }) => {
-    const classes = useStyles();
-    const matches = useMediaQuery('(min-width:600px)');
+const Map = ({
+  setCoords,
+  setBounds,
+  coords,
+  places,
+  setChildClicked,
+  weatherData,
+}) => {
+  const classes = useStyles();
+  const matches = useMediaQuery("(min-width:600px)");
 
+  console.log({places, weatherData})
 
-    return (
-        <div className={classes.mapContainer}>
-            <GoogleMapReact
-                bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY }}
-                defaultCenter={coords}
-                center={coords}
-                defaultZoom={14}
-                margin={[50, 50, 50, 50]}
-                options={{ disableDefaulUI: true, zoomControl:true, styles: mapStyles }}
-                onChange={(e) => {
-
-                    setCoords({ lat: e.center.lat, lng: e.center.lng });
-                    setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
-                }}
-                onChildClick={(child)=> setChildClicked(child)}
-            >
-               {places.length && places.map((place, i) => (
+  return (
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY }}
+        defaultCenter={coords}
+        center={coords}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={{
+          disableDefaulUI: true,
+          zoomControl: true,
+          styles: mapStyles,
+        }}
+        onChange={(e) => {
+          setCoords({ lat: e.center.lat, lng: e.center.lng });
+          setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
+        }}
+        onChildClick={(child) => setChildClicked(child)}
+      >
+        {places.map((place) => (
           <div
             className={classes.markerContainer}
             lat={Number(place.latitude)}
             lng={Number(place.longitude)}
-            key={i}
-            >
-             {!matches?
-             <LocationOnOutlinedIcon color="primary" fontSize="large"/>
-             : (
-                <Paper elevation={3} className={classes.paper}>
-                <Typography className={classes.typography} variant="subtitle2" gutterBottom> {place.name}</Typography>
+            key={place.location_id}
+          >
+            {!matches ? (
+              <LocationOnOutlinedIcon color="primary" fontSize="large" />
+            ) : (
+              <Paper elevation={3} className={classes.paper}>
+                <Typography
+                  className={classes.typography}
+                  variant="subtitle2"
+                  gutterBottom
+                >
+                  {" "}
+                  {place.name}
+                </Typography>
                 <img
                   className={classes.pointer}
-                  src={place.photo ? place.photo.images.large.url : 'https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg'}
+                  src={
+                    place.photo?.images.large.url ||
+                    "https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg"
+                  }
+                  alt=""
                 />
-                 <Rating name="read-only" size="small" value={Number(place.rating)} readOnly />
-                </Paper>
-             )}   
-            </div>
-               ))}
-            
-                {weatherData?.list?.length && weatherData.list.map((data, i) => (
-                    <div key={i} lat={data.coord.lat} lng={data.coord.lon}>
-                      <img src={`http://openweathermap.org/img/w/${data.weather[0].icon}.png`} height="70px" />
-                    </div>
-                  ))}
-            
+                <Rating
+                  name="read-only"
+                  size="small"
+                  value={Number(place.rating)}
+                  readOnly
+                />
+              </Paper>
+            )}
+          </div>
+        ))}
 
-            </GoogleMapReact>
-
-        </div>
-    );
-}
+        {weatherData?.list?.map((data, i) => (
+          <div key={i} lat={data.coord.lat} lng={data.coord.lon}>
+            <img
+              src={`http://openweathermap.org/img/w/${data.weather[0].icon}.png`}
+              height="70px"
+            />
+          </div>
+        ))}
+      </GoogleMapReact>
+    </div>
+  );
+};
 
 export default Map;
