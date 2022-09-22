dfcfedb323d092b6780d63836d0d0f05555b1abe
folorad <fduang@gmail.com>
2022-09-22 12:01:55 +0100

    updated map component with styling

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -11,16 +11,14 @@ const List = ({ places, childClicked, isLoading, type, setType, rating, setRatin
     const [elRefs, setElRefs] = useState([]);
 
     useEffect(() => {
-        const refs = Array(places?.length).fill().map((_, i) => elRefs[i] || createRef());
-
-        setElRefs(refs);
+        setElRefs((refs) => Array(places?.length).fill().map((_, i) => refs[i] || createRef()));
     }, [places]);
 
     //console.log({ childClicked });;
 
     return (
         <div className={classes.container}>
-            <Typography variant='h4'>Restaurant, Hotels & Attractions around you</Typography>
+            <Typography variant='h4'>Food & Dining around you</Typography>
             {isLoading ? (
                 <div className={classes.loading}>
                     <CircularProgress size="5rem" />
@@ -29,7 +27,7 @@ const List = ({ places, childClicked, isLoading, type, setType, rating, setRatin
                 <>
                     <FormControl className={classes.formControl}>
                         <InputLabel>Type</InputLabel>
-                        <Select value={type} onChange={(e) => setType(e.target.value)}>
+                        <Select id="type" value={type} onChange={(e) => setType(e.target.value)}>
                             <MenuItem value="restaurants">Restaurants</MenuItem>
                             <MenuItem value="hotels">Hotels</MenuItem>
                             <MenuItem value="attractions">Attractions</MenuItem>
@@ -37,11 +35,11 @@ const List = ({ places, childClicked, isLoading, type, setType, rating, setRatin
                     </FormControl>
                     <FormControl className={classes.formControl}>
                         <InputLabel>Rating</InputLabel>
-                        <Select value={rating} onChange={(e) => setRating(e.target.value)}>
-                            <MenuItem value={0}>All</MenuItem>
-                            <MenuItem value={3}>Above 3.0</MenuItem>
-                            <MenuItem value={4}>Above 4.0</MenuItem>
-                            <MenuItem value={4.5}>Above 4.5</MenuItem>
+                        <Select id="rating" value={rating} onChange={(e) => setRating(e.target.value)}>
+                            <MenuItem value=" ">All</MenuItem>
+                            <MenuItem value="3">Above 3.0</MenuItem>
+                            <MenuItem value="4">Above 4.0</MenuItem>
+                            <MenuItem value="4.5">Above 4.5</MenuItem>
                         </Select>
                     </FormControl>
                     <Grid container spacing={3} className={classes.list}>
--- src/components/Map/map.jsx
+++ src/components/Map/map.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import GoogleMapReact from "google-map-react";
-import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
 import { Paper, Typography, useMediaQuery } from '@mui/material';
 import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined"
 import  Rating  from '@material-ui/lab/Rating';
@@ -8,7 +7,7 @@ import  Rating  from '@material-ui/lab/Rating';
 import useStyles from './MStyles.js'
 import mapStyles from './mapStyles.js';
 
-const Map = ({setCoordinates, setBounds, coordinates, places, setChildClicked }) => {
+const Map = ({setCoords, setBounds, coords, places, setChildClicked, weatherData }) => {
     const classes = useStyles();
     const matches = useMediaQuery('(min-width:600px)');
 
@@ -17,19 +16,45 @@ const Map = ({setCoordinates, setBounds, coordinates, places, setChildClicked })
         <div className={classes.mapContainer}>
             <GoogleMapReact
                 bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY }}
-                defaultCenter={coordinates}
-                center={coordinates}
+                defaultCenter={coords}
+                center={coords}
                 defaultZoom={14}
                 margin={[50, 50, 50, 50]}
                 options={{ disableDefaulUI: true, zoomControl:true, styles: mapStyles }}
                 onChange={(e) => {
 
-                    setCoordinates({ lat: e.center.lat, lng: e.center.lng });
+                    setCoords({ lat: e.center.lat, lng: e.center.lng });
                     setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
                 }}
-                onChildClick={(child)=> setChildClicked(child) }
+                onChildClick={(child)=> setChildClicked(child)}
             >
-                <h1>Map</h1>
+               {places.length && places.map((place, i) => (
+          <div
+            className={classes.markerContainer}
+            lat={Number(place.latitude)}
+            lng={Number(place.longitude)}
+            key={i}
+            >
+             {!matches?
+             <LocationOnOutlinedIcon color="primary" fontSize="large"/>
+             : (
+                <Paper elevation={3} className={classes.paper}>
+                <Typography className={classes.typography} variant="subtitle2" gutterBottom> {place.name}</Typography>
+                <img
+                  className={classes.pointer}
+                  src={place.photo ? place.photo.images.large.url : 'https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg'}
+                />
+                 <Rating name="read-only" size="small" value={Number(place.rating)} readOnly />
+                </Paper>
+             )}   
+            </div>
+               ))}
+            
+                {weatherData?.list?.length && weatherData.list.map((data, i) => (
+                    <div key={i} lat={data.coord.lat} lng={data.coord.lon}>
+                      <img src={`http://openweathermap.org/img/w/${data.weather[0].icon}.png`} height="70px" />
+                    </div>
+                  ))}
             
 
             </GoogleMapReact>
